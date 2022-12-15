d83261e3977d2a3d50c96297156b34abb97706c3
cho hyein <aifee0518@gmail.com>
2022-12-15 05:45:24 +0000

    [feat] p, label atom component 생성

---
+++ src/components/atoms/p/p.jsx
@@ -0,0 +1,18 @@
+import StyledP from "./styled"
+
+const P = ({ children, margin, padding, display, lineHeight, fontSize, color }) => {
+    return (
+        <StyledP
+        {...{ margin }}
+        {...{ padding }}
+        {...{ display }}
+        {...{ fontSize }}
+        {...{ color }}
+        {...{ lineHeight }}
+        >
+        {children}
+        </StyledP>
+    );
+}
+
+export default P
