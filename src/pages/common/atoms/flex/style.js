91d2892545e3f445614241a89e944e69b7511410
Velo248 <jhpark4884@gmail.com>
2022-12-16 17:21:07 +0900

    [fix][bug] shadow 커스텀가능하게 수정, 여백이 케스케이딩되는 버그 수정

--- src/pages/common/atoms/flex/style.js
+++ src/pages/common/atoms/flex/style.js
@@ -1,4 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const shadow = (option) => {
+  if (option.shadow === 'default')
+    return css`
+      box-shadow: ${option.theme.containers.shadow};
+    `;
+  if (option)
+    return css`
+      box-shadow: ${option.shadow};
+    `;
+  return css`
+    box-shadow: none;
+  `;
+};
 
 export const StyledFlex = styled.div`
   display: flex;
@@ -6,20 +20,12 @@ export const StyledFlex = styled.div`
   height: ${(props) => props.height || 'auto'};
   padding: ${(props) => props.padding || '0'};
   margin: ${(props) => props.margin || '0'};
-  margin-top: ${(props) => props.mt || '0'};
-  margin-bottom: ${(props) => props.mb || '0'};
-  margin-left: ${(props) => props.ml || '0'};
-  margin-right: ${(props) => props.mr || '0'};
-  padding-top: ${(props) => props.pt || '0'};
-  padding-bottom: ${(props) => props.pb || '0'};
-  padding-left: ${(props) => props.pl || '0'};
-  padding-right: ${(props) => props.pr || '0'};
   border-radius: ${(props) => props.radius || props.theme.containers.borderRadius};
-  box-shadow: ${(props) => (!props.shadow ? 'none' : props.theme.containers.shadow)};
   flex-direction: ${(props) => props.flexDirection};
   flex-wrap: ${(props) => props.flexWrap};
-  justify-content: ${(props) => props.justifyContent}
-  align-items: ${(props) => props.alignItems}
-  align-content: ${(props) => props.alignContent}
-  gap: ${(props) => props.gap}
+  justify-content: ${(props) => props.justifyContent};
+  align-items: ${(props) => props.alignItems};
+  align-content: ${(props) => props.alignContent};
+  gap: ${(props) => props.gap};
+  ${(props) => shadow(props)}'
 `;
