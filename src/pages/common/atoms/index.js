d40acf762d17c62f78bc4fb58ed1997679ddc6b7
ub <kimub1204@naver.com>
2022-12-16 23:58:20 +0900

    [feat] 추가 common atom Div 컴포넌트
    * 전달 할 수 있는 props는 children, margin, padding
    , border, borderRadius, fontWeight, fontSize, line
    Height, color, position, right가 있습니다.

--- src/pages/common/atoms/index.js
+++ src/pages/common/atoms/index.js
@@ -1,5 +1,6 @@
 import Button from './button/button';
 import Container from './container/container';
+import Div from './div/div';
 import Flex from './flex/flex';
 import FooterTab from './footerTab/footerTab';
 import Form from './form/form';
@@ -7,4 +8,4 @@ import Image from './image/image';
 import Input from './input/input';
 import Span from './span/span';
 
-export { Form, Image, Input, Span, Container, Flex, Button, FooterTab };
+export { Form, Image, Input, Span, Container, Flex, Button, FooterTab, Div };
