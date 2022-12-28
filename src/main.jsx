e1d4c2b1ee073c80df9a03234d2b5415aab759c1
Velo248 <jhpark4884@gmail.com>
2022-12-28 18:48:59 +0900

    [fix] 태그 비동기 에러 긴급 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+import { Loading } from './pages/common/atoms';
 
 import { Router } from '@/router';
 import { store } from '@/store/';
@@ -14,16 +16,18 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider {...{ theme }}>
-          <GlobalStyle />
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </ThemeProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </Provider>
+    <Suspense fallback={<Loading />}>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider {...{ theme }}>
+            <GlobalStyle />
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </Provider>
+    </Suspense>
   </React.StrictMode>
 );
