5a74bf9ab4f79976fbc278a72beae57baf9dfc11
cho <aifee0518@gmail.com>
2022-12-28 14:55:09 +0900

    [fix] 로그인 회원가입 가격표 전체 수정, 회원정보 수정 기능 추가

--- src/pages/price/atoms/priceTable/priceTable.jsx
+++ src/pages/price/atoms/priceTable/priceTable.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 import { StyledPriceTable } from './styled';
 
-const priceTable =  ({ price ,cate}) => {
+const priceTable =  ({ price ,cate, setSort,sort}) => {
   
 const list = price.map((i,index) => (
     <tr key={index}>
@@ -17,7 +17,7 @@ const list = price.map((i,index) => (
         <thead>
           <tr>
             <th>{cate}</th>
-            <th>가격</th>
+            <th>가격<span style={{float:"right" ,cursor:"pointer"}} onClick={() => setSort(!sort)}>[↕]</span>️</th>
           </tr>
         </thead>
         <tbody>
