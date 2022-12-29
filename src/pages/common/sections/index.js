ada52ae45dd1bfeaf82a5c990ab4211b4f7668c1
Velo248 <jhpark4884@gmail.com>
2022-12-29 17:20:37 +0900

    [stlye] 경로 소문자변경

--- src/pages/common/sections/index.js
+++ src/pages/common/sections/index.js
@@ -3,5 +3,5 @@ import FooterTabs from '@/pages/common/sections/footerTabs/footerTabs';
 import Header from '@/pages/common/sections/header/header';
 import MemoUpdateFormModal from '@/pages/common/sections/memoUpdateFormModal/memoUpdateFormModal';
 import Modal from '@/pages/common/sections/modal/modal';
-import Typography from '@/pages/common/sections/typoGraphy/typoGraphy';
+import Typography from '@/pages/common/sections/typography/typography';
 export { FooterTabs, Typography, Header, Modal, AddressFormModal, MemoUpdateFormModal };
