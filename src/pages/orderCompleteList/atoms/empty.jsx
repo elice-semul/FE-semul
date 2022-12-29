ba3edf7a4f1e44bc2955ba025e529e16b7f8a2d7
Velo248 <jhpark4884@gmail.com>
2022-12-30 01:53:22 +0900

    [style] 아이콘 삭제

--- src/pages/orderCompleteList/atoms/empty.jsx
+++ src/pages/orderCompleteList/atoms/empty.jsx
@@ -1,15 +1,12 @@
-import { BsBasket3 } from 'react-icons/bs';
-
-import { Flex, Span } from '@/pages/common/atoms/index';
+import { Container, Span } from '@/pages/common/atoms/index';
 
 const Empty = () => {
   return (
-    <Flex padding="15% 0 0 0" flexDirection="column" alignItems="center" gap="3rem">
-      <BsBasket3 size="100%" />
-      <Span fontSize="3rem" fontWeight="bold" color="primary" width="100%">
-        세탁물이 없어요!
+    <Container width="100%" margin="15% auto">
+      <Span fontSize="3rem" fontWeight="bold" color="primary">
+        이용내역이 없어요!
       </Span>
-    </Flex>
+    </Container>
   );
 };
 
