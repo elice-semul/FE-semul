4d86070cd068a50db4ecbccca9ae508f06cd820b
Velo248 <jhpark4884@gmail.com>
2022-12-29 17:46:16 +0900

    [fix]경로수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import OrderDetail from './orderDetail/orderDetail';
 import OrderForm from './orderFrom/page/orderForm';
 import Price from './price/page/price';
 import SignUp from './signUp/page/signUp';
-import UserUpdateForm from './userUpdateform/page/UserUpDateForm';
+import UserUpdateForm from './userUpdateform/page/userUpDateForm';
 import Wallet from './wallet/page/wallet';
 
 export {
