465640d060359eeb5af614f2ea8985c91ab3f8ea
Velo248 <jhpark4884@gmail.com>
2022-12-29 16:05:04 +0900

    [style] 소문자로 변경

---
+++ src/router/router.jsx
@@ -0,0 +1,40 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { App } from '@/App';
+import {
+  Home,
+  MyPage,
+  Wallet,
+  OrderDetail,
+  Login,
+  SignUp,
+  Price,
+  OrderForm,
+  OrderComplete,
+  OrderList,
+  UserUpdateForm,
+  NotFound,
+} from '@/pages';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/order/:orderId" element={<OrderDetail />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/price" element={<Price />} />
+        <Route path="/washing" element={<OrderForm />} />
+        <Route path="/washing/complete" element={<OrderComplete />} />
+        <Route path="/history" element={<OrderList />} />
+        <Route path="/updateUser" element={<UserUpdateForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default Router;
